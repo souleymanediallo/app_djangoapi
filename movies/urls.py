7d338970f035663450801211c8e4084e34faf60a
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-03-09 10:39:46 +0100

    register

--- movies/urls.py
+++ movies/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-from .views import MovieView
+from .views import MovieView, RatingView, UserView
 
 router = routers.DefaultRouter()
 router.register('movies', MovieView)
-router.register('ratings', views.RatingView)
+router.register('ratings', RatingView)
+router.register('users', UserView)
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- movies/views.py
+++ movies/views.py
@@ -5,21 +5,29 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 # Create your views here.
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class MovieView(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = [TokenAuthentication]
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'starts' in request.data:
             movie = Movie.objects.get(id=pk)
             starts = request.data['starts']
-            # user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+            # user = User.objects.get(id=1)
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
                 rating.starts = starts
@@ -40,4 +48,14 @@ class MovieView(viewsets.ModelViewSet):
 class RatingView(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update a movie like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create a movie like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
