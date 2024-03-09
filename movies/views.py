193d03aefc8a1c6e700415e1af01c6388689baca
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-03-09 09:46:50 +0100

    update and create

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 # Create your views here.
@@ -8,6 +12,29 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieView(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'starts' in request.data:
+            movie = Movie.objects.get(id=pk)
+            starts = request.data['starts']
+            # user = request.user
+            user = User.objects.get(id=1)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.starts = starts
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except Rating.DoesNotExist:
+                rating = Rating.objects.create(user=user, movie=movie, starts=starts)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'its not working'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingView(viewsets.ModelViewSet):
